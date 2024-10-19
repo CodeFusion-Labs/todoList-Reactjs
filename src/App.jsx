9bb0fb89c0eef162bfcdd2c55db3b99722417e0d
sanaa-ennaji <sanaaennaji93@gmail.com>
2024-10-19 14:57:54 +0100

    add a background and a todolist input

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
+import TodoCard from "./components/TodoCard"
+import TodoInput from "./components/TodoInput"
+import TodoList from "./components/TodoList"
 
 
 function App() {
 
 
   return (
-    <>
-  <div>
-    {4*5}
-  </div>
-    </>
+    <main className="background"> 
+<TodoInput/>
+<TodoCard/>
+<TodoList/>
+    </main>
   )
 }
 
---
+++ src/components/TodoInput.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import '../index.css'
+
+export default function TodoInput() {
+  return (
+   <header >
+    <input type="text" placeholder='enter todo' />
+    <button>add</button>
+   </header>
+  )
+}
